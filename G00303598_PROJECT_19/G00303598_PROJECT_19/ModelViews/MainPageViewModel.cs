4f52f61d104b104a58c17d3b807196c44f7a3c0c
Morgan <G00303598@gmit.ie>
2019-03-25 23:26:20 +0000

    Backend nearly implemented, a lot of ui work to do. Code is also much tidier in this version of the project.

---
+++ G00303598_PROJECT_19/G00303598_PROJECT_19/ModelViews/MainPageViewModel.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+/* Note to self about view models:
+ * View Models
+ * Contains all the methdos and properties
+ * to manipulate the data connected to the fitness list file
+ * and the fitness class
+*/
+
+namespace G00303598_PROJECT_19
+{
+    class MainPageViewModel : BaseViewModel
+    {
+        #region Properties
+        // Using an ObervableCollection as a way of holding data
+        private ObservableCollection<FitnessInfo> myFitnessInfo;
+        public ObservableCollection<FitnessInfo> MyFitnessInfo { get { return myFitnessInfo; }
+            set { SetValue(ref myFitnessInfo, value); }
+        }
+
+        // Unsure why using Fav? -- Remove later? 
+        private FitnessInfo favEntry;
+        public FitnessInfo FavEntry { get { return favEntry; }
+            set { SetValue(ref favEntry, value); }
+        }
+
+        // TODO: Data bind to slOneElement binding context
+        private FitnessInfo selectedEntry;
+        public FitnessInfo SelectedEntry { get { return selectedEntry; }
+            set { SetValue(ref selectedEntry, value); }
+        }
+
+        #endregion
+
+        #region Contructors
+        public MainPageViewModel()
+        {
+            ReadList();
+            // Command Actions
+            SaveListCommand = new Command(SaveList);
+            DeleteFromListCommand = new Command<FitnessInfo>(DeleteOneEntry);
+        }
+        #endregion
+
+        #region Methods
+        public void ReadList()
+        {
+            MyFitnessInfo = FitnessInfo.ReadLocalFitnessData(); // Reads list of fitness info
+        }
+
+        public void SaveList()
+        {
+            // Write observable collection to file
+            FitnessInfo.SaveLocaFitnessData(MyFitnessInfo);
+        }
+
+        public void DeleteOneEntry(FitnessInfo fi)
+        {
+            MyFitnessInfo.Remove(fi);
+            // Need to clear list out
+            SelectedEntry = null;
+        }
+        #endregion
+
+        #region Command Interfaces
+        // ICommand interface with two methods -- Will update for more? 
+        // canExecute, Execute
+        public ICommand SaveListCommand { get; set; }
+        public ICommand DeleteFromListCommand { get; set; }
+        #endregion
+    }
+}
---
+++ G00303598_PROJECT_19/G00303598_PROJECT_19/Models/FitnessInfo.cs
@@ -0,0 +1,147 @@
+﻿using G00303598_PROJECT_19;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Reflection;
+using System.Text;
+using Utils;
+
+namespace G00303598_PROJECT_19
+{
+    public class FitnessInfo : BaseViewModel
+    {
+        #region Contructors
+        // Default contructor
+        public FitnessInfo()
+        {
+
+        }
+        // 8 Param Contructor
+        public FitnessInfo(double currentWeight, double previousWeight, double goalWeight, double caloriesIn, double caloriesInGoal, double waterDrank, double distanceRan, string comment)
+        {
+            _currentWeight = currentWeight;
+            _previousWeight = previousWeight;
+            _goalWeight = goalWeight;
+            _caloriesIn = caloriesIn;
+            _caloriesInGoal = caloriesInGoal;
+            _waterDrank = waterDrank;
+            _distanceRan = distanceRan;
+            _comment = comment;
+        }
+        #endregion
+
+        #region Accessors + Mutators + Member variables
+        private double _currentWeight;
+        public double CurrentWeight { get { return _currentWeight; }
+            set { SetValue(ref _currentWeight, value); }
+        }
+
+        private double _previousWeight;
+        public double PreviousWeight { get { return _previousWeight; }
+            set { SetValue(ref _previousWeight, value); }
+        }
+
+        private double _goalWeight;
+        public double GoalWeight { get { return _goalWeight; }
+            set { SetValue(ref _goalWeight, value); }
+        }
+
+        private double _caloriesIn;
+        public double CaloriesIn { get { return _caloriesIn; }
+            set { SetValue(ref _caloriesIn, value); }
+        }
+
+        private double _caloriesInGoal;
+        public double CaloriesInGoal { get { return _caloriesInGoal; }
+            set { SetValue(ref _caloriesInGoal, value); }
+        }
+
+        private double _waterDrank;
+        public double WaterDrank { get { return _waterDrank; }
+            set { SetValue(ref _waterDrank, value); }
+        }
+
+        private double _distanceRan;
+        public double DistanceRan { get { return _distanceRan; }
+            set { SetValue(ref _distanceRan, value); }
+        }
+
+        private string _comment;
+        public string Comment { get { return _comment; }
+            set { SetValue(ref _comment, value); }
+        }
+        #endregion
+
+        #region File IO
+        public static ObservableCollection<FitnessInfo> ReadLocalFitnessData()
+        {
+            // TODO: Change to type T to make generic
+            // TODO: Make txt files + json info 
+            // TODO: Implemnt MyUtilities
+            ObservableCollection<FitnessInfo> list = new ObservableCollection<FitnessInfo>();
+
+            string fileData; // Using to hold data .. redundant comment
+
+            // Try and read local data first --> "localFitnessInfo.txt"
+            // If first run --> read defualt: "myFitnessInfo.txt"
+
+            try
+            {
+                // Read local data
+                // Path to local folder
+                string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData); // Sandbox folder -- LocalApplicationData
+
+                // Create file name
+                string fileName = Path.Combine(path, MyUtilites.FITNESS_OUTPUT_SAVE_FILE);
+                
+                // Stream reader based on fileName since in local folder
+                using (var reader = new StreamReader(fileName))
+                {
+                    // Reads everything out from file
+                    fileData = reader.ReadToEnd();  // Breakpoint here to find file location
+                }
+            }
+            catch (Exception) //fallback is to read the default file
+            {
+                // Read defualt data
+                // TODO: Write trace file? 
+                var assembly = IntrospectionExtensions.GetTypeInfo(typeof(MainPage)).Assembly;
+
+                // Need stream of data to read from
+                // TODO: Make data folder + populate json
+                Stream stream = assembly.GetManifestResourceStream("G00303598_PROJECT_19.Data.myFitnessInfo.txt");
+
+                using (var reader = new StreamReader(stream))
+                {
+                    fileData = reader.ReadToEnd(); // Reads everything out from file
+                }
+            }
+            //deserialise line here
+            list = JsonConvert.DeserializeObject<ObservableCollection<FitnessInfo>>(fileData);
+                       
+            return list;
+        }
+
+        public static void SaveLocaFitnessData(ObservableCollection<FitnessInfo> list)
+        {
+            // Path name
+            string path = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+
+            // File name
+            string fileName = Path.Combine(path, MyUtilites.FITNESS_OUTPUT_SAVE_FILE);
+
+            // Stream writer --> False: Always overwrite file
+            using (var writer = new StreamWriter(fileName, false))
+            {
+                // Serialise text
+                string stringifiedText = JsonConvert.SerializeObject(list);
+
+                // Write to file
+                writer.WriteLine(stringifiedText);
+            }
+        }
+        #endregion
+    }
+}
